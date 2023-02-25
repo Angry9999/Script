f01e716c22e62dcef81d83aaff75266ea157be93
Angry9999 <126169240+Angry9999@users.noreply.github.com>
2023-02-25 21:59:45 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,13 +1,13 @@
 wait(2)
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Angry Game Gui", "Sentinel")
+local Window = Library.CreateLib("Angry Hub", "Sentinel")
 
 --Main
 local Tab = Window:NewTab("Main")
 local MainSection = Tab:NewSection("Main")
 
-MainSection:NewButton("Fly", "Press once, otherwise it may work poorly.", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Angry9999/Fly/main/Fly?token=GHSAT0AAAAAAB7FSBAIQSVAXOHOH6OM4LSUY7Z66RQ'))()
+MainSection:NewButton("Fly ( Press E)", "Press once, otherwise it may work poorly.", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Angry9999/FlyScript/main/Fly.lua'))()
 end)
 
 MainSection:NewToggle("SuperHuman", "Go fast and jump height", function(state)
@@ -70,5 +70,48 @@ local Other = Window:NewTab("Other")
 local OtherSection = Other:NewSection("Other")
 
 OtherSection:NewButton("Chat Spoofer", "Let's you chat for other people.", function()
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Angry9999/ChatSpoofer/main/ChatSpoofer.lua?token=GHSAT0AAAAAAB7FSBAJEYL3JLGIT2WIC6T4Y72ADLQ'),true))()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Angry9999/ChatSpoofer0/main/ChatSpoofer.lua'),true))()
+end)
+
+OtherSection:NewButton("Esp", "See All People", function()
+    local color = BrickColor.new(50,0,250)
+local transparency = .8
+
+local Players = game:GetService("Players")
+local function _ESP(c)
+  repeat wait() until c.PrimaryPart ~= nil
+  for i,p in pairs(c:GetChildren()) do
+    if p.ClassName == "Part" or p.ClassName == "MeshPart" then
+      if p:FindFirstChild("shit") then p.shit:Destroy() end
+      local a = Instance.new("BoxHandleAdornment",p)
+      a.Name = "shit"
+      a.Size = p.Size
+      a.Color = color
+      a.Transparency = transparency
+      a.AlwaysOnTop = true    
+      a.Visible = true    
+      a.Adornee = p
+      a.ZIndex = true    
+
+    end
+  end
+end
+local function ESP()
+  for i,v in pairs(Players:GetChildren()) do
+    if v ~= game.Players.LocalPlayer then
+      if v.Character then
+        _ESP(v.Character)
+      end
+      v.CharacterAdded:Connect(function(chr)
+        _ESP(chr)
+      end)
+    end
+  end
+  Players.PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(function(chr)
+      _ESP(chr)
+    end)  
+  end)
+end
+ESP()
 end)
